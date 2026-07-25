36d2d01af9153e38e5d6004620420dc09d474810
husseinameerk-collab <274803070+husseinameerk-collab@users.noreply.github.com>
2026-07-24 20:52:35 -0500

    Add Television class

---
+++ television.py
@@ -0,0 +1,44 @@
+class Television:
+    MIN_VOLUME = 0
+    MAX_VOLUME = 2
+    MIN_CHANNEL = 0
+    MAX_CHANNEL = 3
+
+    def __init__(self):
+        self.__status = False
+        self.__muted = False
+        self.__volume = Television.MIN_VOLUME
+        self.__channel = Television.MIN_CHANNEL
+
+    def power(self):
+        self.__status = not self.__status
+
+    def mute(self):
+        if self.__status:
+            self.__muted = not self.__muted
+
+    def channel_up(self):
+        if self.__status:
+            self.__channel += 1
+            if self.__channel > Television.MAX_CHANNEL:
+                self.__channel = Television.MIN_CHANNEL
+
+    def channel_down(self):
+        if self.__status:
+            self.__channel -= 1
+            if self.__channel < Television.MIN_CHANNEL:
+                self.__channel = Television.MAX_CHANNEL
+
+    def volume_up(self):
+        if self.__status:
+            self.__muted = False
+            self.__volume = min(self.__volume + 1, Television.MAX_VOLUME)
+
+    def volume_down(self):
+        if self.__status:
+            self.__muted = False
+            self.__volume = max(self.__volume - 1, Television.MIN_VOLUME)
+
+    def __str__(self):
+        volume = Television.MIN_VOLUME if self.__muted else self.__volume
+        return f'Power = {self.__status}, Channel = {self.__channel}, Volume = {volume}'
